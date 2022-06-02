604acb7868905b57d997c7075c92c4580f2568ca
greatstep93 <greatstep93@gmail.com>
2022-06-03 00:09:23 +0400

    WORKED!!!

--- src/main/java/ru/greatstep/spring/boot_security/models/User.java
+++ src/main/java/ru/greatstep/spring/boot_security/models/User.java
@@ -50,7 +50,7 @@ public class User implements UserDetails {
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    @Column(name = "Roles")
+
     private Collection<Role> roles;
 
     public String getRolesView() {
